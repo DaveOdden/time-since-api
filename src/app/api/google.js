2869c46f56363be63c760e2441ca54ad8da71694
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2025-01-30 20:15:05 -0500

    google auth

--- src/app/api/google.js
+++ src/app/api/google.js
@@ -15,14 +15,12 @@ export default async function handler(req, res) {
       audience: process.env.GOOGLE_CLIENT_ID,
     });
 
-    console.log(ticket)
     const payload = ticket.getPayload(); // Extract user info
     const { sub, name, email, picture } = payload;
 
     // TODO: Save user to database if not exists
     res.status(200).json({ id: sub, name, email, picture });
-  } catch (error) {
-    console.log(error)
+  } catch {
     res.status(401).json({ error: "Invalid token" });
   }
 }
